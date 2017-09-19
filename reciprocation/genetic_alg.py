4a571448d863af591b14b529f6b73c663be2f7d4
Steven Damer <damer@maximus.cs.umn.edu>
2017-09-18 19:37:56 -0500

    added dials

--- reciprocation/genetic_alg.py
+++ reciprocation/genetic_alg.py
@@ -23,23 +23,23 @@ class randomlinearstrat:
         else:
             self.stratlist=[]
 
-    def mutate(self,other):
+    def mutate(self,other,rate=.5,mag=1):
         result=randomlinearstrat(True)
         result.stratlist=[x for x in self.stratlist]
         # add/delete point(s)
-        if random.random()<.5**len(self.stratlist):
+        if random.random()<rate*.5**len(self.stratlist):
             newpoint=(2*random.random()-1,2*random.random()-1)
             bisect.insort_left(result.stratlist,newpoint)
-        elif random.random()<.25-.5**len(self.stratlist):
+        elif random.random()<rate*(.25-.5**len(self.stratlist)):
             i=random.randint(1,len(self.stratlist)-2)
             result.stratlist=result.stratlist[:i]+result.stratlist[i+1:]
         # change point(s)
-        if random.random()<.25:
+        if random.random()<rate*.25:
             i=random.randint(0,len(result.stratlist)-1)
-            result.stratlist[i]=(result.stratlist[i][0],min(1,max(-1,result.stratlist[i][1]+random.normalvariate(0,.5))))
+            result.stratlist[i]=(result.stratlist[i][0],min(1,max(-1,result.stratlist[i][1]+mag*random.normalvariate(0,.5))))
         # crossover
         if other is not None:
-            if random.random()<.5:
+            if random.random()<rate*.5:
                 p=random.random()
                 i=bisect.bisect(result.stratlist,(p,None))
                 j=bisect.bisect(other.stratlist,(p,None))
@@ -51,18 +51,22 @@ class randomlinearstrat:
         w=(move-self.stratlist[r-1][0])/(self.stratlist[r][0]-self.stratlist[r-1][0])
         return (1-w)*self.stratlist[r-1][1]+w*self.stratlist[r][1]
 
-def evaluate(strat,learner,iterations,discountfactor=1.0):
+def evaluate(strat,learner,iterations,discountfactor=1.0,repetitions=1):
     curdiscount=1.0
     normalize=0.0
     score=0.0
     move=None
-    for i in range(iterations):
-        move=learner.respond(move)
-        score=score+curdiscount*move
-        move=strat.respond(move)
-        score=score+curdiscount*math.sqrt(1-move**2)
-        normalize=normalize+curdiscount
-        curdiscount = curdiscount * discountfactor
+    for j in range(repetitions):
+        for i in range(iterations):
+            move=learner.respond(move)
+            score=score+curdiscount*move
+            move=strat.respond(move)
+            score=score+curdiscount*math.sqrt(1-move**2)
+            normalize=normalize+curdiscount
+            curdiscount = curdiscount * discountfactor
+        learner.reset()
+        move=None
+        curdiscount=1.0
     return score/normalize
 
 def weightedselect(weights,objects):
@@ -77,8 +81,9 @@ def weightedselect(weights,objects):
 class player:
     def __init__(self,learner,radial=False,**kwargs):
         self.radial=radial
-        if learner=="UCT":
-            self.learner=players.UCTlearner(kwargs['c'])
+        self.learnertype=learner
+        self.kwargs=kwargs
+        self.reset()
 
     def respond(self,move):
         if move is None:
@@ -92,6 +97,11 @@ class player:
         else:
             self.lastpayoff=math.sqrt(1-self.lastmove**2)
             return self.lastmove
+
+    def reset(self):
+        if self.learnertype=="UCT":
+            self.learner=players.UCTlearner(self.kwargs['c'])
+
 
 class learnerfactory:
     def __init__(self,radial=False,**kwargs):
@@ -107,9 +117,11 @@ class genepool:
         self.poolsize=poolsize
         self.discountfactor=.99
         self.learnerfactory=learnerfactory(False,c=.25)
+        self.darwin=math.e
 
     def rungeneration(self):
-        scores=[math.exp(evaluate(r,self.learnerfactory.mklearner(),1000,self.discountfactor)) for r in self.genepool]
+        scores=[evaluate(r,self.learnerfactory.mklearner(),1000,self.discountfactor) for r in self.genepool]
+        scores=[self.darwin**x for x in scores]
         newpool=[]
         for i in range(self.poolsize):
             newpool.append(weightedselect(scores,self.genepool).mutate(weightedselect(scores,self.genepool)))
