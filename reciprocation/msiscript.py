b2bb5582f96811b522b419c42de91fbff4a05a7c
Hampswitch <steven.damer@gmail.com>
2018-02-28 22:59:00 -0600

    added multiprocessing

--- reciprocation/msiscript.py
+++ reciprocation/msiscript.py
@@ -2,6 +2,7 @@
 import learningstrategies as learners
 import genetic_alg as ga
 import teachingstrategies as teachers
+import multiprocessing
 
 rvals=[0.0,.156,.309,.454,.588,.707,.809,.891,.951,.988,1.0]
 opplist=[(.988,-.691,0.0),(.951,-.381,0.0),(.891,-.091,0.0),(.809,0.0,0.0),
@@ -10,11 +11,21 @@ opplist=[(.988,-.691,0.0),(.951,-.381,0.0),(.891,-.091,0.0),(.809,0.0,0.0),
          (.707,-1.0,-1.0),(.588,-1.0,-1.0),(.454,-1.0,-1.0),(.309,-1.0,-1.0),(.156,-1.0,-1.0),(0.0,-1.0,-1.0)]
 
 
+
+def processopp(opp):
+    threshhold=opp[0]
+    zero=opp[1]
+    negone=opp[2]
+    output=""
+    for startmove in [-x for x in rvals[-1:0:-1]]+rvals:
+        for initresponse in [-x for x in rvals[-1:0:-1]]+rvals:
+            learner=learners.player(learner=learners.UCTlearner(c=1.0),startmove=startmove)
+            teacher=teachers.simpleteacher(threshhold,zero,negone,override=[initresponse])
+            result=ga.evaluate(learner,teacher,1000,.99,1000)
+            output=output+", ".join([str(x) for x in [threshhold,zero,negone,startmove,initresponse,result[2],result[0]]])+"\n"
+
 if __name__=="__main__":
-    for (threshhold,zero,negone) in opplist:
-        for startmove in [-x for x in rvals[-1:0:-1]]+rvals:
-            for initresponse in [-x for x in rvals[-1:0:-1]]+rvals:
-                learner=learners.player(learner=learners.UCTlearner(c=1.0),startmove=startmove)
-                teacher=teachers.simpleteacher(threshhold,zero,negone,override=[initresponse])
-                result=ga.evaluate(learner,teacher,1000,.99,1000)
-                print ", ".join([str(x) for x in [threshhold,zero,negone,startmove,initresponse,result[2],result[0]]])
+    pool=multiprocessing.Pool(processes=10)
+    result=pool.map(processopp,opplist)
+    for r in result:
+        print r
