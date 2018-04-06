8358bf38a1bc5c682787fa460196f477b22dbcea
Hampswitch <steven.damer@gmail.com>
2018-04-06 18:11:30 -0500

    fixed print call

--- reciprocation/msiscript.py
+++ reciprocation/msiscript.py
@@ -43,7 +43,7 @@ if __name__=="__main__":
     pool=multiprocessing.Pool(processes=poolsize)
     result=pool.map(processopp,opponentlist(threshhold))
     for r in result:
-        print r
+        print(r)
 
 if __name__=="test__main__": # Test speed
     for opp in opponentlist(.707):
